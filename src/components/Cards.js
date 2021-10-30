f1fc95ca7144c26e2affce4a5aec94a0cc6f871c
esionascimento <esio.392@protonmail.com>
2021-10-30 17:22:20 -0400

    add folder components, pages / add files cards, home

---
+++ src/components/Cards.js
@@ -0,0 +1,17 @@
+export const Cards = () => {
+  return (
+    <>
+      <div>
+        <p>Cada litro de tinta é capaz de pintar 5 metros quadrados</p>
+        <p>Não considerar teto nem piso</p>
+        <p>As variações de tamanho das latas de tinta são: 0,5L; 2,5L; 3,6L; 18L</p>
+      </div>
+      <div>
+        <div>Primeira Parede</div>
+        <div>Segunda Parede</div>
+        <div>Terceira Parede</div>
+        <div>Quarta Parede</div>
+      </div>
+    </>
+  );
+}
