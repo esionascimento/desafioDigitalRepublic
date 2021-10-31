db5b685bbf5a42333fa5a237ce22cca2c070cbff
esionascimento <esio.392@protonmail.com>
2021-10-31 00:06:26 -0400

    organizaçao dos folders pages, create folder dashboard, create conteudo home

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import './Home.css'
+
+function Home() {
+  return (
+    <>
+      <div className="box">
+        <div className="container">
+          <div className="left">
+            <h1>Calculadora de Tintas</h1>
+            <p></p>
+          </div>
+          <div className="right">
+          </div>
+        </div>
+      </div>
+      <footer className="rodape">
+        Desafio 
+      </footer>
+    </>
+  );
+}
+
+export default Home;
