a26c944d2ded889fb5fcad1ede0c500420f9aa22
esionascimento <esio.392@protonmail.com>
2021-11-01 08:34:39 -0400

    create page Resultado

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { Home } from './pages/Home/Home.js';
 import { Cadastro } from './pages/Cadastro/Cadastro.js';
 import { Dashboard } from './pages/Dashboard/Dashboard.js';
+import { Resultado } from './pages/Resultado/Resultado.js';
 
 import './App.css';
 
@@ -14,6 +15,7 @@ const App = () => {
         <Route exact path="/" component={Home} />
         <Route exact path="/cadastro" component={Cadastro} />
         <Route exact path="/dashboard" component={Dashboard} />
+        <Route exact path="/dashboard/resultado" component={Resultado} />
       </Switch>
     </BrowserRouter>
   );
