8fe1c53230233d18df225adaa74c89191c78d9a6
esionascimento <esio.392@protonmail.com>
2021-11-02 17:46:57 -0400

    finalizando

--- src/tests/Home.test.js
+++ src/tests/Home.test.js
@@ -3,42 +3,32 @@ import { Router } from 'react-router-dom';
 import { render, fireEvent, screen } from "@testing-library/react";
 import { createMemoryHistory } from "history";
 import { Home } from "../pages/Home/Home.js";
+import renderWithRouter from './RenderRouter';
 import '@testing-library/jest-dom';
 
 describe("Página Home.js", () => {
   const history = createMemoryHistory();
   
   it("Testando: 2(Dois) inputs na página Home", () => {
-    const { getByTestId } = render(
-      <Router history={history}>
-        <Home />
-      </Router>
-    );
+    renderWithRouter(<Home />);
 
-    expect(getByTestId('input-email')).toBeEmptyDOMElement();
-    expect(getByTestId('input-password')).toBeEmptyDOMElement();
+    expect(screen.getByTestId('input-email')).toBeEmptyDOMElement();
+    expect(screen.getByTestId('input-password')).toBeEmptyDOMElement();
   });
   it("Testando: Rota atual está na página home", () => {
-    const { getByText } = render(
-      <Router history={history}>
-        <Home />
-      </Router>
-    );
+    renderWithRouter(<Home />);
 
-    const titulo = getByText(/Calculadora de Tintas/i);
+    const titulo = screen.getByText(/Calculadora de Tintas/i);
     expect(titulo).toBeInTheDocument();
     expect(history.location.pathname).toBe('/');
   });
   it("Test: Redirecionar para a página dashboard ao clicar no botão Entrar", async () => {
-    const { getByTestId } = render(
-      <Router history={history}>
-        <Home />
-      </Router>
-    );
-    const email = getByTestId('input-email');
+    renderWithRouter(<Home />);
+    
+    const email = screen.getByTestId('input-email');
     fireEvent.change(email, {target: {value: 'register@register.com.br'}});
 
-    const password = getByTestId('input-password');
+    const password = screen.getByTestId('input-password');
     fireEvent.change(password, {target: {value: '123456'}});
 
     const button = screen.getByRole('button', {name: /Entrar/i});
