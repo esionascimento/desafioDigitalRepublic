a36f91cc8d9c1a37d8b385bb735d34414007b003
esionascimento <esio.392@protonmail.com>
2021-11-03 03:32:37 -0400

    new button de voltar na pagina resultado

--- src/pages/Resultado/Resultado.js
+++ src/pages/Resultado/Resultado.js
@@ -1,18 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
+import  { Redirect } from 'react-router-dom';
 
 import './Resultado.css';
 
 export function Resultado() {
   const { resultadoTotalParedeM2 } = useSelector((state) => state.simulador);
   const { lata05, lata25, lata36, lata18 } = useSelector((state) => state.calcular);
+  const [redirect, setRedirect] = useState(false);
+
+  function onClickResult() {
+    setRedirect(true);
+  }
   
+  if (redirect) {
+    return <Redirect to="/dashboard" />
+  }
+
   return (
     <div className="conteiner">
       <h2>Resultado</h2>
       <div className="conteiner-body">
         <div>
-          <p>Total de parede: {resultadoTotalParedeM2}m^2</p>
+          <p>Total de parede: {(resultadoTotalParedeM2).toFixed(2)}m^2</p>
         </div>
         <div>
           {lata05 > 0 && <p>Precisa de {lata05} latas de 0.5L</p>}
@@ -20,6 +30,7 @@ export function Resultado() {
           {lata36 > 0 && <p>Precisa de {lata36} latas de 3.6L</p>}
           {lata18 > 0 && <p>Precisa de {lata18} latas de 18L</p>}
         </div>
+        <button className="btn btn-primary" onClick={onClickResult}>Voltar</button>
       </div>
       <footer className="rodape">
         Resultado desenvolvido em pair programming com João Oliveira
